fe8780acee67f4e70021c19c6efc09fa2ded6a39
secondsunrise <secondsunrise@users.noreply.github.com>
2016-07-05 09:43:24 -0600

    Create white-line-remover.py

---
+++ white-line-remover.py
@@ -0,0 +1,14 @@
+# Open the file
+with open("your_file_name", mode="r") as f:
+    data = f.readlines()
+
+# Get rid of the newline character
+for i in range(0, len(data)):
+    data[i] = data[i].replace("\n","")
+
+# Filter out the blank strings ("") from the list
+data = filter(None, data)
+
+with open("your_new_file_name", mode="w") as f:
+    for element in data:
+        f.write(email + "\n")
